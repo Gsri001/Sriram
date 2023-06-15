8ba59f0328b0c40381ebbfc4d43b582b9cdf3a17
Gsri001 <134907305+Gsri001@users.noreply.github.com>
2023-06-15 09:54:56 +0530

    Add files via upload

---
+++ js course/index.js
@@ -0,0 +1,28 @@
+const person= {
+     namee:"ganesh",
+     age:88,
+     sibilin:{
+        son:"gan",
+        age:99
+     } 
+}
+function person(){
+    age=10
+    console.log(person);
+}
+function()
+
+
+// for(let ke in person){
+//     console.log(ke +":"+  person[ke])
+// }
+    
+// }
+// let colours=["red","blue","green"]
+// for (ket in colours){
+//     console.log(ket)
+//     console.log(colours[ket])
+// }
+// for(colour of colours){
+//     console.log(colour)
+// }
